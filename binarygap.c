5e8cac2d09f9a428299fedf0b05f2211e3e4e4ee
Nathan <nathan@mattick.name>
2016-06-17 21:08:51 +0100

    Bitshift only

--- binarygap.c
+++ binarygap.c
@@ -1,14 +1,7 @@
 /*
-First attempt.
-start - set true when first bit==1 encountered
-gap - incremented on each bit==zero
-max - records max gap
-when started, if bit==1 then update max and clear gap
-
-fails: doesn't find enough bits in the input int.
+Using bit shift only. 
+sizeof(N) appeared to cause issues.
 */
-
-#define BIT(N,i) (0x1 & ((N)>>(i)))
 
 int solution(int N) {
     // write your code in C99 (gcc 4.8.2)
@@ -16,33 +9,29 @@ int solution(int N) {
         return(0);
     }
     
-    int l = 4*sizeof(N);
-    int i;
     int gap = 0;
     int max = 0;
     int start = 0;
-    for (i = 0 ; i < l ; i++) {
+    while (N > 0) {
         if (!start) {
-            if (BIT(N,i)) {
+            if (0x1 & N) {
                 start = 1;
             }
-            continue;
         }
         else {
-            if (BIT(N,i)) {
+            if (0x1 & N) {
                 if (gap > 0) {
                     max = (max > gap) ? max : gap;
                     gap = 0;
-                }
-                else {
-                    continue;
                 }
             }
             else {
                 gap++;
             }
         }
+        N >>= 1;
     }
     return(max);    
 }
+
 
